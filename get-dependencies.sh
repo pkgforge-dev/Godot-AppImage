62d8e0ab22a43025c7155c2dd665a7d492b6abae
Samueru-sama <xdglawyer@outlook.com>
2026-09-20 14:32:44 -0400

    build godot manually, add `aarch64` releases

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -2,25 +2,79 @@
 
 set -eu
 
-ARCH=$(uname -m)
-
-echo "Installing package dependencies..."
+echo "Installing build dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm godot
+pacman -Syu --noconfirm \
+	brotli            \
+	graphite          \
+	libdecor          \
+	libglvnd          \
+	libogg            \
+	libpng            \
+	libspeechd        \
+	libtheora         \
+	libvorbis         \
+	libwebp           \
+	libwslay          \
+	libxcursor        \
+	libxi             \
+	libxinerama       \
+	openxr            \
+	scons             \
+	wayland-protocols \
+	yasm              \
+	zlib              \
+	zstd
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Building Godot..."
+echo "---------------------------------------------------------------"
+git clone --filter=blob:none --no-checkout https://github.com/godotengine/godot ./godot && (
+	cd ./godot
 
-# If the application needs to be manually built that has to be done down here
+	# Build the latest stable release
+	TAG=$(git tag --list '*-stable' --sort=-v:refname | head -n 1)
+	git checkout "$TAG"
+	VERSION="${TAG%-stable}"
+	echo "$VERSION" > ~/version
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+	# Allow the engine to run on CPUs without SSE4.2 and POPCNT
+	patch -p1 < ../patches/0001-do-not-require-sse4.2-and-popcnt.patch
+
+	scons \
+		-j"$(nproc)"              \
+		platform=linuxbsd         \
+		target=editor             \
+		production=yes            \
+		use_llvm=no               \
+		werror=no                 \
+		builtin_certs=yes         \
+		builtin_brotli=no         \
+		builtin_embree=yes        \
+		builtin_freetype=no       \
+		builtin_graphite=no       \
+		builtin_harfbuzz=yes      \
+		builtin_icu4c=yes         \
+		builtin_libogg=no         \
+		builtin_libpng=no         \
+		builtin_libtheora=no      \
+		builtin_libvorbis=no      \
+		builtin_libwebp=no        \
+		builtin_mbedtls=yes       \
+		builtin_miniupnpc=yes     \
+		builtin_openxr=no         \
+		builtin_pcre2=no          \
+		builtin_pcre2_with_jit=no \
+		builtin_zlib=no           \
+		builtin_zstd=no
+)
+
+mkdir -p ./AppDir
+cp -v ./godot/bin/godot.linuxbsd.editor.*    /usr/bin/godot
+cp -v ./godot/misc/logo/icon.png             ./AppDir/godot.png
+cp -v ./godot/misc/dist/linux/*Godot.desktop ./AppDir/Godot.desktop
+
+rm -rf ./godot
--- make-appimage.sh
+++ make-appimage.sh
@@ -3,13 +3,10 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q godot | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/256x256/apps/godot.png
-export DESKTOP=/usr/share/applications/org.godotengine.Godot.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
